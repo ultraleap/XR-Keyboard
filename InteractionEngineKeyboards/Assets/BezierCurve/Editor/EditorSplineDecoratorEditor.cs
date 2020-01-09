bb5f5045e21ccee00c4538ce5c711b7e1eaeed66
Pip Turner <pip.turner@ultrahaptics.com>
2020-01-09 15:14:15 +0000

    Linearly space child objects along a curve using align

---
+++ InteractionEngineKeyboards/Assets/BezierCurve/Editor/EditorSplineDecoratorEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EditorSplineDecorator))]
+public class EditorSplineDecoratorEditor : Editor {
+
+	public override void OnInspectorGUI () {
+        if (GUILayout.Button("Align"))
+        {
+            EditorSplineDecorator esd = (EditorSplineDecorator)serializedObject.targetObject;
+            esd.AlignObjects();
+        }
+        DrawDefaultInspector();
+	}
+}
