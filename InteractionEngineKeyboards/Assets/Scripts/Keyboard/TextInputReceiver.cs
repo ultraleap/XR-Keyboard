c3868a34434c25294a6e85df89dfaf44b697a8ef
Pip Turner <pip.turner@ultraleap.com>
2020-01-20 14:24:05 +0000

    Added punch keyboard's work picker to "Keyboard - straight" scene

---
+++ InteractionEngineKeyboards/Assets/Scripts/Keyboard/TextInputReceiver.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class TextInputReceiver : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro _textMesh;
+    [SerializeField]
+    private Levenshtein _levenshtein;
+    [SerializeField]
+    NGramGenerator _wordPredictor;
+
+    private string actualText = "";
+    public string text
+    {
+        get { return actualText; }
+        set
+        {
+            actualText = value;
+            UpdateTM();
+            _levenshtein.RunAutoComplete(actualText);
+        }
+    }
+
+    public void Append(char input)
+    {
+        text += input;
+    }
+
+    public void Backspace()
+    {
+        if (text.Length > 0)
+        {
+            text = text.Substring(0, text.Length - 1);
+        }
+    }
+
+    public void ReplaceWord(string correctWord)
+    {
+        List<string> inputText = new List<string>();
+        StringBuilder builder = new StringBuilder();
+        string input = text;
+        string[] parts = input.Split(' ');
+        parts = input.Split(' ').Take(parts.Length - 1).ToArray();
+
+        for (int i = 0; i < parts.Length; i++)
+        {
+            inputText.Add(parts[i]);
+        }
+
+        inputText.Add(correctWord);
+
+        foreach (string w in inputText)
+        {
+            builder.Append(w).Append(" ");
+        }
+        text = builder.ToString();
+        _wordPredictor.PredictNextWords(correctWord);
+    }
+
+    void UpdateTM()
+    {
+        _textMesh.text = text + "|";
+    }
+}
