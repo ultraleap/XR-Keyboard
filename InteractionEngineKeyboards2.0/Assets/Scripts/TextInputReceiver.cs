0a99166472cda9abe6a10d561e3e82022e115b97
pip.turner <pip.turner@ultraleap.com>
2021-02-04 11:50:38 +0000

    Remove unneeded code from textinput scripts

--- InteractionEngineKeyboards2.0/Assets/Scripts/TextInputReceiver.cs
+++ InteractionEngineKeyboards2.0/Assets/Scripts/TextInputReceiver.cs
@@ -9,10 +9,6 @@ public class TextInputReceiver : MonoBehaviour
 {
     [SerializeField]
     private TextMeshPro _textMesh;
-    //[SerializeField]
-    //private Levenshtein _levenshtein;
-    //[SerializeField]
-    //NGramGenerator _wordPredictor;
 
     private string actualText = "";
     public string text
@@ -28,14 +24,6 @@ public class TextInputReceiver : MonoBehaviour
     public void Append(char input)
     {
         text += input;
-        //if (input == ' ')
-        //{
-        //    _levenshtein.ClearLabelPredictions();
-        //}
-        //else
-        //{
-        //    StartCoroutine(_levenshtein.RunAutoComplete(text));
-        //}
     }
 
     public void Backspace()
@@ -43,38 +31,12 @@ public class TextInputReceiver : MonoBehaviour
         if (text.Length > 0)
         {
             text = text.Substring(0, text.Length - 1);
-            //StartCoroutine(_levenshtein.RunAutoComplete(text));
         }
     }
 
     public void Reset()
     {
         text = string.Empty;
-        //_levenshtein.ClearLabelPredictions();
-    }
-
-    public void ReplaceWord(string correctWord)
-    {
-        List<string> inputText = new List<string>();
-        StringBuilder builder = new StringBuilder();
-        string input = text;
-        string[] parts = input.Split(' ');
-        parts = input.Split(' ').Take(parts.Length - 1).ToArray();
-
-        for (int i = 0; i < parts.Length; i++)
-        {
-            inputText.Add(parts[i]);
-        }
-
-        inputText.Add(correctWord);
-
-        foreach (string w in inputText)
-        {
-            builder.Append(w).Append(" ");
-        }
-        text = builder.ToString();
-        //_levenshtein.ClearLabelPredictions();
-        //StartCoroutine(_wordPredictor.PredictNextWords(correctWord));
     }
 
     void UpdateTM()
