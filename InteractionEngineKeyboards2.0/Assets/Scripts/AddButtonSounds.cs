d0b32b1d3fc202fcf315cd19c48aea09569b975d
pip.turner <pip.turner@ultraleap.com>
2021-02-04 13:36:57 +0000

    Add sounds & more obvious colours

---
+++ InteractionEngineKeyboards2.0/Assets/Scripts/AddButtonSounds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leap.Unity;
+using Leap.Unity.Interaction;
+
+public class AddButtonSounds : MonoBehaviour
+{
+    public Transform ButtonParent;
+    public AudioClip hoverSound;
+    public AudioClip downSound;
+    public AudioClip upSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InteractionButton[] interactionButtons = ButtonParent.transform.GetComponentsInChildren<InteractionButton>(true);
+        foreach (InteractionButton interactionButton in interactionButtons)
+        {
+            if (interactionButton.gameObject.GetComponent<AudioSource>() == null)
+            {
+                interactionButton.gameObject.AddComponent<AudioSource>();
+            }
+
+            if (interactionButton.gameObject.GetComponent<ButtonSounds>() == null)
+            {
+                ButtonSounds bSounds = interactionButton.gameObject.AddComponent<ButtonSounds>();
+                if (hoverSound != null) { bSounds.hoverSound = hoverSound; }
+                if (downSound != null) { bSounds.downSound = downSound; }
+                if (upSound != null) { bSounds.upSound = upSound; }
+            }
+        }
+    }
+}
---
+++ InteractionEngineKeyboards2.0/Assets/Scripts/SetSimpleInteractionGlowColoursInChildren.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSimpleInteractionGlowColoursInChildren : MonoBehaviour
+{
+    [Header("InteractionBehaviour Colors")]
+    public Color defaultColor = Color.Lerp(Color.black, Color.white, 0.1F);
+    public Color suspendedColor = Color.red;
+    public Color hoverColor = Color.Lerp(Color.black, Color.white, 0.7F);
+    public Color primaryHoverColor = Color.Lerp(Color.black, Color.white, 0.8F);
+
+    [Header("InteractionButton Colors")]
+    [Tooltip("This color only applies if the object is an InteractionButton or InteractionSlider.")]
+    public Color pressedColor = Color.white;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SimpleInteractionGlow[] simpleInteractionGlows = transform.GetComponentsInChildren<SimpleInteractionGlow>(true);
+        foreach (SimpleInteractionGlow simpleInteractionGlow in simpleInteractionGlows)
+        {
+            simpleInteractionGlow.defaultColor = defaultColor;
+            simpleInteractionGlow.suspendedColor = suspendedColor;
+            simpleInteractionGlow.hoverColor = hoverColor;
+            simpleInteractionGlow.primaryHoverColor = primaryHoverColor;
+            simpleInteractionGlow.pressedColor = pressedColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
