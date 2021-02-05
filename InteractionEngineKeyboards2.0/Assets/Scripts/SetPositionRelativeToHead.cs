8a66f0c9a40b3b96c593af45297a2f0927e73a46
pip.turner <pip.turner@ultraleap.com>
2021-02-05 09:28:06 +0000

    Sort out keyboard positioning

---
+++ InteractionEngineKeyboards2.0/Assets/Scripts/SetPositionRelativeToHead.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPositionRelativeToHead : MonoBehaviour
+{
+    public Transform head;
+    public Transform keyboard;
+    public Vector3 DistanceFromHead;
+    public Vector3 Angles;
+
+    public void SetPosition()
+    {
+        Debug.Log("SET POS");
+        keyboard.rotation = Quaternion.identity * Quaternion.Euler(Angles);
+        keyboard.position = head.position + DistanceFromHead;
+    }
+}
