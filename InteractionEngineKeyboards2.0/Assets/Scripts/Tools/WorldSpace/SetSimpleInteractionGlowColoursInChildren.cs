6444db9b893f372d74fe360e0dc27528553325fe
pip.turner <pip.turner@ultraleap.com>
2021-02-15 14:20:40 +0000

    Update glow

--- InteractionEngineKeyboards2.0/Assets/Scripts/Tools/WorldSpace/SetSimpleInteractionGlowColoursInChildren.cs
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/WorldSpace/SetSimpleInteractionGlowColoursInChildren.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Leap.Unity;
+using TMPro;
 using UnityEngine;
 
 public class SetSimpleInteractionGlowColoursInChildren : MonoBehaviour
 {
+    public Color TextColor = Color.Lerp(Color.black, Color.white, 0.2F);
     [Header("InteractionBehaviour Colors")]
     public Color defaultColor = Color.Lerp(Color.black, Color.white, 0.1F);
     public Color suspendedColor = Color.red;
     public Color hoverColor = Color.Lerp(Color.black, Color.white, 0.7F);
     public Color primaryHoverColor = Color.Lerp(Color.black, Color.white, 0.8F);
+
+    public bool UseHover = false;
+    public bool UsePrimaryHover = true;
 
     [Header("InteractionButton Colors")]
     [Tooltip("This color only applies if the object is an InteractionButton or InteractionSlider.")]
@@ -24,12 +31,23 @@ public class SetSimpleInteractionGlowColoursInChildren : MonoBehaviour
             simpleInteractionGlow.hoverColor = hoverColor;
             simpleInteractionGlow.primaryHoverColor = primaryHoverColor;
             simpleInteractionGlow.pressedColor = pressedColor;
+            simpleInteractionGlow.useHover = UseHover;
+            simpleInteractionGlow.usePrimaryHover = UsePrimaryHover;
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        SimpleInteractionGlowImage[] SimpleInteractionGlowImages = transform.GetComponentsInChildren<SimpleInteractionGlowImage>(true);
+        foreach (SimpleInteractionGlowImage simpleInteractionGlowImage in SimpleInteractionGlowImages)
+        {
+            simpleInteractionGlowImage.defaultColor = defaultColor;
+            simpleInteractionGlowImage.suspendedColor = suspendedColor;
+            simpleInteractionGlowImage.hoverColor = hoverColor;
+            simpleInteractionGlowImage.primaryHoverColor = primaryHoverColor;
+            simpleInteractionGlowImage.pressedColor = pressedColor;
+            simpleInteractionGlowImage.useHover = UseHover;
+            simpleInteractionGlowImage.usePrimaryHover = UsePrimaryHover;
+        }
 
+        transform.GetComponents<TextMeshPro>().ToList().ForEach(tmp => tmp.color = TextColor);
+        transform.GetComponents<TextMeshProUGUI>().ToList().ForEach(tmp => tmp.color = TextColor);
     }
 }
