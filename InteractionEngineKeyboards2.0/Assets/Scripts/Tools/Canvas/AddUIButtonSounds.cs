90cc841ce200f4ce6208b9bb003529f709ba7f6b
pip.turner <pip.turner@ultraleap.com>
2021-02-15 13:20:24 +0000

    Rename AddButtonSounds to AddUIButtonSounds

--- InteractionEngineKeyboards2.0/Assets/Scripts/Tools/Canvas/AddUIButtonSounds.cs
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/Canvas/AddUIButtonSounds.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AddButtonSounds : MonoBehaviour
+public class AddUIButtonSounds : MonoBehaviour
 {
     public Transform UIParent;
     public AudioClip hoverSound;
