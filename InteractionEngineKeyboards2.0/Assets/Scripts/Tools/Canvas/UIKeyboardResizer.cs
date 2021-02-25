9f0e9e97569ca21129f9edf0bb6c3531fde4e6a3
pip.turner <pip.turner@ultraleap.com>
2021-02-25 10:28:53 +0000

    Remove unneeded usings

--- InteractionEngineKeyboards2.0/Assets/Scripts/Tools/Canvas/UIKeyboardResizer.cs
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/Canvas/UIKeyboardResizer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Leap.Unity.Interaction;
 using NaughtyAttributes;
 using UnityEditor;
 using UnityEngine;
