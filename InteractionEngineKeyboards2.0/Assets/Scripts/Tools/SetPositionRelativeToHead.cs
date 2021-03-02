47cae15b80d04b7f7cea99497d955ade28c27c23
pip.turner <pip.turner@ultraleap.com>
2021-03-02 11:34:18 +0000

    Add transform manager between set pos button & transform of keyboard

--- InteractionEngineKeyboards2.0/Assets/Scripts/Tools/SetPositionRelativeToHead.cs
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/SetPositionRelativeToHead.cs
@@ -5,29 +5,14 @@ using UnityEngine;
 public class SetPositionRelativeToHead : MonoBehaviour
 {
     public Transform head;
-    public Transform keyboard;
+    public TargetTransformManager TargetTransformManager;
     public Vector3 DistanceFromHead;
     public Vector3 Angles;
 
-    private Vector3 targetLocation;
-    private Quaternion targetRotation;
-
-    private void Start()
-    {
-        targetLocation = keyboard.position;
-        targetRotation = keyboard.rotation;
-    }
-    private void Update()
-    {
-        if (keyboard.position != targetLocation || keyboard.rotation != targetRotation)
-        {
-            keyboard.position = Vector3.Lerp(keyboard.position, targetLocation, Time.deltaTime * 30);
-            keyboard.rotation = Quaternion.Lerp(keyboard.rotation, targetRotation, Time.deltaTime * 30);
-        }
-    }
     public void SetPosition()
     {
-        targetRotation = Quaternion.identity * Quaternion.Euler(Angles);
-        targetLocation = head.position + DistanceFromHead;
+        TargetTransformManager.TargetRotation = Quaternion.identity * Quaternion.Euler(Angles);
+        TargetTransformManager.TargetPosition = head.position + DistanceFromHead;
     }
+
 }
---
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/TargetTransformManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetTransformManager : MonoBehaviour
+{
+    [HideInInspector] public Vector3 TargetPosition;
+    [HideInInspector] public Quaternion TargetRotation;
+    public float lerpSpeed = 30;
+
+    private void Start() {
+        TargetPosition = transform.position;
+        TargetRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (transform.position != TargetPosition || transform.rotation != TargetRotation)
+        {
+            transform.position = Vector3.Lerp(transform.position, TargetPosition, Time.deltaTime * lerpSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, TargetRotation, Time.deltaTime * lerpSpeed);
+        }
+    }
+}
