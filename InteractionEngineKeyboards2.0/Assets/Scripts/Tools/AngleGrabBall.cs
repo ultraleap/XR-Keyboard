3b7627162c407ff93123304de8175cce7f4fa22b
Peter Nancollis <peter.nancollis@ultrahaptics.com>
2021-03-03 17:30:58 +0000

    Add a grab rotation gizmo

---
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/AngleGrabBall.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+
+
+[RequireComponent(typeof(InteractionBehaviour))]
+public class AngleGrabBall : MonoBehaviour
+{
+    public Transform targetObject;
+    public Transform grabGimbal;
+    public float lerpSpeed = 20;
+
+    public float offsetDistance = 0.25f;
+
+    public float startAngle = 35;
+
+    public Vector3 localOffset;
+    private InteractionBehaviour behaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        behaviour = GetComponent<InteractionBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!behaviour.isGrasped)
+        {
+            Vector3 targetPosition = targetObject.TransformPoint(localOffset);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * lerpSpeed);
+        }
+    }
+
+    public void UpdateAngle()
+    {
+        Vector3 targetPosition = grabGimbal.InverseTransformPoint(transform.position);
+        targetPosition.x = 0;
+        Vector3 targetDirection = targetPosition - grabGimbal.InverseTransformPoint(targetObject.position);
+        Quaternion rotation = Quaternion.FromToRotation(grabGimbal.up, targetDirection);
+        targetObject.localRotation = rotation;
+    }
+}
