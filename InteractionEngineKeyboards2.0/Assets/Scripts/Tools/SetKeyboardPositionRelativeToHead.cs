71adb0e180c4d84cff5aee0ab22deba97b9b658d
pip.turner <pip.turner@ultraleap.com>
2021-03-03 10:47:30 +0000

    increase sprite size & add comment

--- InteractionEngineKeyboards2.0/Assets/Scripts/Tools/SetKeyboardPositionRelativeToHead.cs
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/SetKeyboardPositionRelativeToHead.cs
@@ -21,6 +21,9 @@ public class SetKeyboardPositionRelativeToHead : MonoBehaviour
     }
     public void SetPosition()
     {
+        //As the grab ball's pivot point isn't in the centre of the keyboard, 
+        // we want to work out how far we'd need to move the keyboard to be offset from the head
+        // and then apply that offset to the grab ball
         Vector3 offset = KeyboardCentre.position - (head.position + DistanceFromHead);
         GrabBall.position -= offset;
         reorient = true;
