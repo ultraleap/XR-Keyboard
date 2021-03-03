b0ba962f85ca5f762bad11705d3c0b3e1289efad
pip.turner <pip.turner@ultraleap.com>
2021-03-03 10:28:29 +0000

    wait a frame to reorient when resetting position

--- InteractionEngineKeyboards2.0/Assets/Scripts/Tools/SetKeyboardPositionRelativeToHead.cs
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/SetKeyboardPositionRelativeToHead.cs
@@ -9,11 +9,21 @@ public class SetKeyboardPositionRelativeToHead : MonoBehaviour
     public GrabGimbal GrabGimbal;
     public Transform KeyboardCentre;
     public Vector3 DistanceFromHead;
+    private bool reorient = false;
+
+    private void Update()
+    {
+        if (reorient)
+        {
+            GrabGimbal.UpdateTargetRotation();
+            reorient = false;
+        }
+    }
     public void SetPosition()
     {
         Vector3 offset = KeyboardCentre.position - (head.position + DistanceFromHead);
         GrabBall.position -= offset;
-        GrabGimbal.UpdateTargetRotation();
+        reorient = true;
     }
 
 }
