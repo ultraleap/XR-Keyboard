1831b1b26a750ca523ff5bdac9a76728a625b1df
Peter Nancollis <peter.nancollis@ultrahaptics.com>
2021-03-04 11:55:52 +0000

    Draw a rotation gizmo indicating the angle

--- InteractionEngineKeyboards2.0/Assets/Scripts/Tools/AngleGrabBall.cs
+++ InteractionEngineKeyboards2.0/Assets/Scripts/Tools/AngleGrabBall.cs
@@ -5,6 +5,7 @@ using Leap.Unity.Interaction;
 
 
 [RequireComponent(typeof(InteractionBehaviour))]
+[RequireComponent(typeof(LineRenderer))]
 public class AngleGrabBall : MonoBehaviour
 {
     public Transform targetObject;
@@ -16,12 +17,15 @@ public class AngleGrabBall : MonoBehaviour
     public float startAngle = 35;
 
     public Vector3 localOffset;
+
+    private LineRenderer rotationGizmo;
     private InteractionBehaviour behaviour;
 
     // Start is called before the first frame update
     void Start()
     {
         behaviour = GetComponent<InteractionBehaviour>();
+        rotationGizmo = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -31,6 +35,11 @@ public class AngleGrabBall : MonoBehaviour
         {
             Vector3 targetPosition = targetObject.TransformPoint(localOffset);
             transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * lerpSpeed);
+            HideRotationGizmo();
+        }
+        else
+        {
+            DrawRotationGizmo();
         }
     }
 
@@ -41,5 +50,43 @@ public class AngleGrabBall : MonoBehaviour
         Vector3 targetDirection = targetPosition - grabGimbal.InverseTransformPoint(targetObject.position);
         Quaternion rotation = Quaternion.FromToRotation(grabGimbal.up, targetDirection);
         targetObject.localRotation = rotation;
+    }
+
+    private void HideRotationGizmo()
+    {
+        if ( rotationGizmo.positionCount > 0)
+        rotationGizmo.positionCount--;
+    }
+
+    private void DrawRotationGizmo()
+    {
+        rotationGizmo.enabled = true;
+        rotationGizmo.positionCount = 64;
+
+        Keyframe[] keys = new Keyframe[rotationGizmo.positionCount];
+        float currentRotationTime = Vector3.SignedAngle(Vector3.up, targetObject.up, targetObject.right) / 360;
+        float dir = Mathf.Sign(currentRotationTime);
+        
+        currentRotationTime = Mathf.Abs(currentRotationTime);
+
+        startAngle = currentRotationTime;
+        for (int i = 0; i < rotationGizmo.positionCount; i++)
+        {
+            float time = (i / (float)rotationGizmo.positionCount) ;
+            Vector3 position = new Vector3() {
+                x = 0,
+                y = Mathf.Cos(time * 2 * Mathf.PI) * localOffset.magnitude,
+                z = -dir * Mathf.Sin(time * 2 * Mathf.PI) * localOffset.magnitude
+            };
+            
+            rotationGizmo.SetPosition(i, targetObject.TransformPoint(position));
+            
+            bool innerAngle = time > currentRotationTime;
+            float width = innerAngle ? Mathf.Lerp(1, 0.1f,(time - currentRotationTime) * 10f) : 1;
+
+            keys[i] = new Keyframe(time, width);
+        }
+        rotationGizmo.widthCurve = new AnimationCurve(keys);
+
     }
 }
