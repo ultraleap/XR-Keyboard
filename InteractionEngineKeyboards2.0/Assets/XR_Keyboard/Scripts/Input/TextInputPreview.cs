4fc0f23ee0a2253597306b4035ff166f30d2546e
Peter Nancollis <peter.nancollis@ultrahaptics.com>
2021-03-30 10:53:05 +0100

    Add optional word limit to preview and enable on type a mole

--- InteractionEngineKeyboards2.0/Assets/XR_Keyboard/Scripts/Input/TextInputPreview.cs
+++ InteractionEngineKeyboards2.0/Assets/XR_Keyboard/Scripts/Input/TextInputPreview.cs
@@ -8,6 +8,8 @@ public class TextInputPreview : MonoBehaviour
 
     private string text;
 
+    public int maxWords = 0;
+
     private void OnEnable()
     {
         if (_UITextMesh == null) { _UITextMesh = GetComponent<TextMeshProUGUI>(); }
@@ -21,6 +23,32 @@ public class TextInputPreview : MonoBehaviour
     public void UpdatePreview(string newText)
     {
         text = newText;
+        if (maxWords > 0)
+        {
+            text = Truncate(text);
+        }
+
         _UITextMesh.text = text + "|";
+    }
+
+    private string Truncate(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        
+        if (words.Length > maxWords)
+        {
+            string result = "";
+            int offset = words.Length - maxWords;
+            for(int i = 0; i < maxWords; i++)
+            {
+                result += words[i + offset] + " ";
+            }
+
+            return result.Substring(0, result.Length - 2);
+        }
+        else
+        {
+            return sentence;
+        }
     }
 }
