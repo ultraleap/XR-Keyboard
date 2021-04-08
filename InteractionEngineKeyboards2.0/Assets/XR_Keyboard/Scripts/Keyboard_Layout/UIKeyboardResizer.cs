81316beb682b265cbb01581387f6d2f5fc92bbb3
pip.turner <pip.turner@ultraleap.com>
2021-04-08 08:37:10 +0100

    Sort Usings

--- InteractionEngineKeyboards2.0/Assets/XR_Keyboard/Scripts/Keyboard_Layout/UIKeyboardResizer.cs
+++ InteractionEngineKeyboards2.0/Assets/XR_Keyboard/Scripts/Keyboard_Layout/UIKeyboardResizer.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using NaughtyAttributes;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
