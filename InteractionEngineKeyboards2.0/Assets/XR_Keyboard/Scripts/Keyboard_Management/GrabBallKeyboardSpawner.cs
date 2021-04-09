5afee3c79fe70d5b9cd6f0f7f4333eb76ab5c7bb
pip.turner <pip.turner@ultraleap.com>
2021-04-09 11:05:07 +0100

    Change keyboard spawner to be basic class to inherit from

---
+++ InteractionEngineKeyboards2.0/Assets/XR_Keyboard/Scripts/Keyboard_Management/GrabBallKeyboardSpawner.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GrabBallKeyboardSpawner : KeyboardSpawner
+{
+    public enum RelativeTo
+    {
+        HEAD,
+        TEXT_FIELD
+    }
+    public Transform head;
+
+    public GameObject PrefabParent;
+    public GrabBall GrabBall;
+    public Transform KeyboardCentre;
+    public Vector3 DistanceFromHead = new Vector3(0, -0.385f, 0.4f);
+    public RelativeTo PositionRelativeTo = RelativeTo.TEXT_FIELD;
+    public RelativeTo RotationRelativeTo = RelativeTo.HEAD;
+    private bool keyboardActive = false;
+    private bool despawning = false;
+    private Vector3 offset;
+    private Rigidbody grabBallRigidBody;
+
+
+    private void Start()
+    {
+        if (head == null) head = Camera.main.transform;
+        offset = PrefabParent.transform.position - KeyboardCentre.position;
+        grabBallRigidBody = GrabBall.GetComponent<Rigidbody>();
+        DespawnKeyboard();
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(WaitThenDespawn());
+    }
+
+    public override void SpawnKeyboard(Transform currentlySelected)
+    {
+        if (keyboardActive)
+        {
+            return;
+        }
+        else
+        {
+            keyboardActive = true;
+        }
+        if (despawning)
+        {
+            return;
+        }
+        PrefabParent.SetActive(keyboardActive);
+
+        if (PositionRelativeTo == RelativeTo.HEAD)
+        {
+            SetPositionRelativeTo(head.forward);
+        }
+        else if (PositionRelativeTo == RelativeTo.TEXT_FIELD)
+        {
+            SetPositionRelativeTo(currentlySelected.transform.position);
+        }
+
+        if (RotationRelativeTo == RelativeTo.HEAD)
+        {
+            GrabBall.UpdateTargetRotation();
+        }
+        else if (RotationRelativeTo == RelativeTo.TEXT_FIELD)
+        {
+            GrabBall.targetRotation = currentlySelected.transform.rotation;
+        }
+    }
+    public override void DespawnKeyboard()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            StartCoroutine(WaitThenDespawn());
+        }
+    }
+
+    private IEnumerator WaitThenDespawn()
+    {
+        // Because OnDeselect happens on the last frame that the object is the currently selected, 
+        // we need to wait for the next frame to check if we should still despawn,
+        // as the next selected object may have flagged to spawn again
+        keyboardActive = false;
+        despawning = true;
+        yield return null;
+
+        if (!keyboardActive)
+        {
+            if (GrabBall != null)
+            {
+                PrefabParent.gameObject.SetActive(keyboardActive);
+                despawning = false;
+            }
+        }
+    }
+
+    private void SetPositionRelativeTo(Vector3 _relativePosition)
+    {
+        Vector3 directionVector = Vector3.Normalize(_relativePosition - head.position);
+        Vector3 newPosition = head.position + (directionVector * (DistanceFromHead.z + offset.z));
+        newPosition.y = head.position.y + DistanceFromHead.y + offset.y;
+        SetPosition(newPosition);
+    }
+
+    private void SetPosition(Vector3 _newPosition)
+    {
+        grabBallRigidBody.position = _newPosition;
+    }
+}
--- InteractionEngineKeyboards2.0/Assets/XR_Keyboard/Scripts/Keyboard_Management/KeyboardSpawner.cs
+++ InteractionEngineKeyboards2.0/Assets/XR_Keyboard/Scripts/Keyboard_Management/KeyboardSpawner.cs
@@ -1,116 +1,25 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class KeyboardSpawner : MonoBehaviour
 {
-    public enum RelativeTo
+    // Start is called before the first frame update
+    void Start()
     {
-        HEAD,
-        TEXT_FIELD
-    }
-    public Transform head;
-
-    public GameObject PrefabParent;
-    public GrabBall GrabBall;
-    public Transform KeyboardCentre;
-    public Vector3 DistanceFromHead = new Vector3(0, -0.385f, 0.4f);
-    public RelativeTo PositionRelativeTo = RelativeTo.TEXT_FIELD;
-    public RelativeTo RotationRelativeTo = RelativeTo.HEAD;
-    private bool keyboardActive = false;
-    private bool despawning = false;
-    private Vector3 offset;
-    private Rigidbody grabBallRigidBody;
-
-
-    private void Start()
-    {
-        if (head == null) head = Camera.main.transform;
-        offset = PrefabParent.transform.position - KeyboardCentre.position;
-        grabBallRigidBody = GrabBall.GetComponent<Rigidbody>();
-        DespawnKeyboard();
+        
     }
 
-    private void OnDestroy()
+    // Update is called once per frame
+    void Update()
     {
-        StopCoroutine(WaitThenDespawn());
+        
     }
 
-    public void SpawnKeyboard(Transform currentlySelected)
-    {
-        if (keyboardActive)
-        {
-            return;
-        }
-        else
-        {
-            keyboardActive = true;
-        }
-        if (despawning)
-        {
-            return;
-        }
-        PrefabParent.SetActive(keyboardActive);
+    public virtual void SpawnKeyboard(Transform currentlySelected){
 
-        if (PositionRelativeTo == RelativeTo.HEAD)
-        {
-            SetPositionRelativeTo(head.forward);
-        }
-        else if (PositionRelativeTo == RelativeTo.TEXT_FIELD)
-        {
-            SetPositionRelativeTo(currentlySelected.transform.position);
-        }
-
-        if (RotationRelativeTo == RelativeTo.HEAD)
-        {
-            GrabBall.UpdateTargetRotation();
-        }
-        else if (RotationRelativeTo == RelativeTo.TEXT_FIELD)
-        {
-            GrabBall.targetRotation = currentlySelected.transform.rotation;
-        }
     }
-    public void DespawnKeyboard()
-    {
-        if (gameObject.activeInHierarchy)
-        {
-            StartCoroutine(WaitThenDespawn());
-        }
-    }
-
-    private IEnumerator WaitThenDespawn()
-    {
-        // Because OnDeselect happens on the last frame that the object is the currently selected, 
-        // we need to wait for the next frame to check if we should still despawn,
-        // as the next selected object may have flagged to spawn again
-        keyboardActive = false;
-        despawning = true;
-        yield return null;
-
-        if (!keyboardActive)
-        {
-            if (GrabBall != null)
-            {
-                PrefabParent.gameObject.SetActive(keyboardActive);
-                despawning = false;
-            }
-        }
-    }
-
-    private void SetPositionRelativeTo(Vector3 _relativePosition)
-    {
-        Vector3 directionVector = Vector3.Normalize(_relativePosition - head.position);
-        Vector3 newPosition = head.position + (directionVector * (DistanceFromHead.z + offset.z));
-        newPosition.y = head.position.y + DistanceFromHead.y + offset.y;
-        SetPosition(newPosition);
-    }
-
-    private void SetPosition(Vector3 _newPosition)
-    {
-        grabBallRigidBody.position = _newPosition;
+    public virtual void DespawnKeyboard(){
+        
     }
 }
